9ac46e4ee59aa8fcdb48631644d5ba7149b30d66
Sakshi <sakshipatil0344@gmail.com>
2023-11-01 09:00:44 +0530

    NavBar

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+// Navigation.js
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav className="navigation">
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Videos</li>
+        <li>Summaries</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
